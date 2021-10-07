844b54600e6c86d919f287f3891ad2cf756ce1eb
preetirini <91046032+preetirini@users.noreply.github.com>
2021-10-07 09:31:02 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,27 @@
+
+public class Activity1 {
+	
+	
+	
+  
+
+	public static void main(String[] args) {
+		
+		Car bmw = new Car();
+		// TODO Auto-generated method stub
+		bmw.make = 2014;
+		bmw.color = "black" ;
+		bmw.transmission = "Manual" ;
+		
+		bmw.displayCharacteristics();
+		bmw.accelarate();
+		bmw.brake();
+
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,23 @@
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp = 0;
+		int arr[] =  {10, 77, 10, 54, -11, 10};
+		
+	for (int i=0; i <= arr.length-1 ; i++) {
+		
+		if(arr[i]==10) {
+		
+	
+		temp += arr[i];
+		
+		
+	}
+		
+
+	}
+	System.out.println("Sum: " + temp);
+	}
+}
